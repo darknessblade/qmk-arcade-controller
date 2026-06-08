e422f6398b4f7ba240af198a280586a648737ccd
darknessblade <darknessbladers@gmail.com>
2026-06-08 18:52:10 +0200

    Add files via upload

---
+++ arcade/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD1, 0x47, 0xEF, 0x04, 0xFA, 0x4D, 0xD3, 0x8C}  // Generate unique UID
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#define VIAL_COMBO_ENABLE yes
+
+// Joystick settings
+#define JOYSTICK_AXIS_COUNT 2
+#define JOYSTICK_BUTTON_COUNT 32
